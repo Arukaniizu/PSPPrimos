16717e247602d2917b55b781d3a8b904e0497f13
Arukaniizu <59884052+Arukaniizu@users.noreply.github.com>
2020-10-14 17:56:59 +0200

    a

--- src/Hilo.java
+++ src/Hilo.java
@@ -5,11 +5,12 @@ import java.math.BigInteger;
 public class Hilo  extends Thread{
 	public int numero = 0;
 	public int numero2 = 0;
+	public int cont = 0;
 	
-	public Hilo(int numero, int numero2) {
-		
+	public Hilo(int numero, int numero2, int cont) {
 		this.numero = numero;
 		this.numero2 = numero2;
+		this.cont = cont;
 	}
 	public void run() {
 		calculo();
@@ -21,8 +22,9 @@ public class Hilo  extends Thread{
 			bi = new BigInteger(String.valueOf(i));
 			if (bi.isProbablePrime(1) ) {
 				System.out.println(i +"   "+ Thread.currentThread().getName());
+				cont ++;
 			}
-
+			
 		}
 	}
 }
--- src/Hilos.java
+++ src/Hilos.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 
 public class Hilos {
 	public static void main(String[] args) {
-		final int NUM = 100000;
+		final int NUM = 100;
 		int inicio = 0;
 		int rango = 0;
 		int rangototal = 0;
 		int numeroHilos = 0;
+		int cont = 0;
 		// pedir el numero de hilos y calcular los numeros primos del 1 al 100000
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Inserta el numero de hilos que van a participar");
@@ -15,14 +16,14 @@ public class Hilos {
 		rango = NUM / numeroHilos;
 		rangototal = rango;
 		Hilo hilo;
-
 		for (int i = 0; i < numeroHilos; i++) {
-			hilo = new Hilo(inicio, rangototal);
+			hilo = new Hilo(inicio, rangototal, cont);
 			hilo.start();
 			inicio = rangototal;
 			rangototal = rangototal + rango;
 		}
-
+		
+		
 		sc.close();
 
 	}
